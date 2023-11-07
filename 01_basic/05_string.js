c5d4a1bc18366554e60d9a38a3cb935c42eda856
prashpp <55101831+prashpp@users.noreply.github.com>
2023-11-07 18:25:56 +0000

    new

---
+++ 01_basic/05_string.js
@@ -0,0 +1,26 @@
+const name = "Prashant";
+const repoCount = 50
+
+// console.log(` Hello My name is ${name} an repoCount is ${repoCount} `)
+
+const getname = new String("HelloWorld")
+
+// console.log(getname.length)
+// console.log(getname.toUpperCase())
+// console.log(getname.charAt(2))
+// console.log(getname.indexOf('l'))
+
+// const newString = getname.substring(0,4)
+// console.log(newString)
+
+// const anotherString = getname.slice(-1, 5)
+// console.log(anotherString)
+
+// const newStringOne = "             newString         ";
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://prashant%20parmar.com"
+console.log(url.replace("%20", "-"))
+
+console.log(url.includes("prashant"))
